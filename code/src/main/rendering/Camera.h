510c528ec1b3d6214f3c6e1d0fd9c64001a4f506
Titus <titusawolfe@gmail.com>
2026-03-15 16:55:46 -0500

    Add Camera class.

---
+++ code/src/main/rendering/Camera.h
@@ -0,0 +1,15 @@
+//
+// Created by Titus Wolfe on 3/15/26.
+//
+
+#ifndef SDLAWESOMENESS_CAMERA_H
+#define SDLAWESOMENESS_CAMERA_H
+#include "../math/Vector.h"
+
+
+class Camera {
+    Vector position;
+};
+
+
+#endif //SDLAWESOMENESS_CAMERA_H
